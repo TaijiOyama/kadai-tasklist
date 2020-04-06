2030400b29a62ddc4df43b24cf1b97d0054771bd
taiji.oyama <ForGitHub2020@gmail.com>
2020-04-06 07:29:28 +0000

    完成2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  
   def index
       @tasks = Task.all
   end
@@ -46,7 +47,7 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
+
 
 private
 
@@ -54,3 +55,5 @@ private
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+end
